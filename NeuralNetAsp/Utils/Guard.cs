3bc18969bf2e3e4132f547fd675cc1dbc7f5e531
Daniel Armyr <daniel.armyr@nextory.com>
2023-03-01 22:36:45 +0530

    Added strict style validation and addressed reported issues.

--- NeuralNetAsp/Utils/Guard.cs
+++ NeuralNetAsp/Utils/Guard.cs
@@ -1,19 +1,19 @@
 namespace NeuralNetAsp.Utils
 {
-  class Guard
+  sealed class Guard
   {
     public static void Check(bool condition)
     {
       if (!condition)
       {
-        throw new System.Exception("Condition failed");
+        throw new System.ArgumentException("Condition failed");
       }
     }
     public static void CheckEqual(object one, object two)
     {
       if (!one.Equals(two))
       {
-        throw new System.Exception("Expected " + one + " to equal " + two);
+        throw new System.ArgumentException("Expected " + one + " to equal " + two);
       }
     }
   }
