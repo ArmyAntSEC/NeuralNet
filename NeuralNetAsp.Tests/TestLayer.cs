4f194c8f854e3b373b6d0518d2463daadf4ab20f
Daniel Armyr <daniel.armyr@nextory.com>
2023-03-01 22:26:29 +0530

    Removed unneeded comments

--- NeuralNetAsp.Tests/TestLayer.cs
+++ NeuralNetAsp.Tests/TestLayer.cs
@@ -7,14 +7,6 @@ namespace NeuralNetAsp.Tests;
 [TestClass]
 public class TestLayer
 {
-  //Each layer has N perceptrons
-
-  //There exists a fake alyer that is just the input
-
-  //A Layer takes another layer as its input.
-
-  //Every perceptron in the layer reads the output array of the input layer and computes its own output.
-
   [TestMethod]
   public void TestCreateInputLayer()
   {
