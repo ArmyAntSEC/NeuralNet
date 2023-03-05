07da85e5f504e3c1584ed7fba8b6f08bbe33070a
Daniel Armyr <daniel.armyr@nextory.com>
2023-03-05 13:06:52 +0530

    Added a proper training call to the CDK deployment test as well.

--- scripts/utils/cdk_deploy.sh
+++ scripts/utils/cdk_deploy.sh
@@ -9,4 +9,8 @@ cdk deploy --require-approval never
 
 sleep 10
 
+echo "Heartbeat:"
 curl --fail http://neura-neura-jjeraxh3bz3p-691205240.eu-west-1.elb.amazonaws.com/api/heartbeat/ || exit 1
+
+echo "Training:"
+curl -X POST http://neura-neura-jjeraxh3bz3p-691205240.eu-west-1.elb.amazonaws.com:80/api/neural/ -H 'Content-Type: application/json' -d '{"input":[-0.33,-0.33,-0.33,-0.33,-0.33,0.69,0.94,0.5,0.75,0.67,0,1,1,0,1,1,0,0,1,1],"output":[1,1,0,1,0]}'
