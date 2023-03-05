e208bc99cd35f2bd78f74df11f1abadb7b3335ee
Daniel Armyr <daniel.armyr@nextory.com>
2023-03-05 07:26:10 +0530

    Docker deploy script now fully working

--- NeuralNetAsp/scripts/scripts.sh
+++ NeuralNetAsp/scripts/scripts.sh
@@ -4,8 +4,6 @@ docker build -t neuralnetasp .
 docker images 
 docker run -d -p 80:80 --name neuralnetasp neuralnetasp
 
-until [ "`docker inspect -f {{.State.Running}} neuralnetasp`"=="true" ]; do
-    sleep 0.1;
-done;
+sleep 10
 
 curl http://localhost:80/api/values/
