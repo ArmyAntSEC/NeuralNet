46cc80f51cdbfcf333b39a2af1ce232bde8566bd
Daniel Armyr <daniel.armyr@nextory.com>
2023-03-06 10:34:55 +0530

    First API test working.

---
+++ NeuralNetAsp.ApiTests/TestNeuralNetApi.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http.Headers;
+using System.Diagnostics;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace NeuralNetAsp.ApiTests
+{
+  [TestClass]
+  public class NeuralNetApiTests
+  {
+
+    string baseUrl = "http://localhost:5000/api";
+
+    static readonly HttpClient httpClient = new HttpClient();
+
+    [TestMethod]
+    public void TestHeartbeatApi()
+    {
+      var url = baseUrl + "/heartbeat";
+      using (HttpResponseMessage message = httpClient.GetAsync(url).Result)
+      {
+        Assert.AreEqual(true, message.IsSuccessStatusCode);
+
+        var deserializedObject = JsonSerializer.Deserialize<HeartbeatResponse>(message.Content.ReadAsStringAsync().Result);
+        Assert.AreEqual("Alive!", deserializedObject.Status);
+      }
+    }
+  }
+  public class HeartbeatResponse
+  {
+    [JsonPropertyName("status")]
+    public String Status { get; set; }
+
+    public HeartbeatResponse(String status)
+    {
+      Status = status;
+    }
+  }
+}
