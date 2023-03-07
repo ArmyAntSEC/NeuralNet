569612bc2c7868e83e5331ea019a24c9504996e7
Daniel Armyr <daniel.armyr@nextory.com>
2023-03-07 11:22:04 +0530

    Now does full auto testing of both local deployment and clud deployment.

--- scripts/test_and_deploy_local.sh
+++ scripts/test_and_deploy_local.sh
@@ -3,9 +3,11 @@ echo "*** Starting unit tests"
 dotnet test -l "console;verbosity=detailed" ../NeuralNetAsp.Tests/NeuralNetAsp.Tests.csproj || exit
 
 # Build and deploy a container locally
+echo "*** Building container"
 sh ./utils/docker_build.sh
 
-echo "Giving Docker 10s to start"
+echo "*** Giving Docker 10s to start"
 sleep 10
 
-dotnet test -l "console;verbosity=detailed" ../NeuralNetAsp.ApiTests/NeuralNetAsp.ApiTests.csproj || exit
+echo "*** Doing API tests"
+NEURAL_NET_BASE_URL=http://localhost:80 dotnet test -l "console;verbosity=detailed" ../NeuralNetAsp.ApiTests/NeuralNetAsp.ApiTests.csproj || exit
--- scripts/utils/cdk_deploy.sh
+++ scripts/utils/cdk_deploy.sh
@@ -1,20 +1,13 @@
-#Run the below in the NeuralNetCdk directory
-#dotnet build #In what directory?
-pwd
 cd ../NeuralNetCdk
 
-pwd
+echo "*** Bootstrapping CDK. Only actually needed once"
 cdk bootstrap
+
+echo "*** DEploying with CDK"
 cdk deploy --require-approval never
 
-sleep 10
+echo "*** Waiting 20 seconds for system to come online fully.!"
+sleep 20
 
-echo "Heartbeat:"
-curl --fail http://neura-neura-jjeraxh3bz3p-691205240.eu-west-1.elb.amazonaws.com:80/api/heartbeat/ || exit 1
-
-echo "Training:"
-curl -X POST http://neura-neura-jjeraxh3bz3p-691205240.eu-west-1.elb.amazonaws.com:80/api/trainer/ -H 'Content-Type: application/json' -d '{"input":[-0.33,-0.33,-0.33,-0.33,-0.33,0.69,0.94,0.5,0.75,0.67,0,1,1,0,1,1,0,0,1,1],"output":[1,1,0,1,0]}'
-
-echo "Predict"
-curl -X POST http://neura-neura-jjeraxh3bz3p-691205240.eu-west-1.elb.amazonaws.com:80/api/predictor/ -H 'Content-Type: application/json' -d '{"layerOneWeights":[0.20501795434531928,4.851008781645798,-5.094595716697624,-3.173856984876353,-0.3819715353811023,5.729700533135146,-7.097541432816082,-3.6949973939179026,-0.1169280643114341,-3.895180491131896,2.054754866046295,1.697961953959847],"layerTwoWeights":[5.44573619851985,5.522509842250541,-4.927694841147059],"input":[-0.33,0.69,0,1]}'
-echo "The above should be close to 1.0"
+echo "*** Doing API tests on deployed system"
+NEURAL_NET_BASE_URL=http://neura-neura-jjeraxh3bz3p-691205240.eu-west-1.elb.amazonaws.com:80 dotnet test -l "console;verbosity=detailed" ../NeuralNetAsp.ApiTests/NeuralNetAsp.ApiTests.csproj || exit
